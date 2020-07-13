09b303257064c5aa008666934e0842e6651dc4e3
Tom Jacob <feedback@trialrules.com>
2020-07-13 10:51:09 -0500

    Initial Commit

---
+++ Tabs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tabs
+//
+//  Created by Tom Jacob on 7/13/20.
+//  Copyright © 2020 Tom Jacob. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
